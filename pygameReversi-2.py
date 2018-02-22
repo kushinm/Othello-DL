5be34e793664bea725660bebadb7c4548be418ea
Squidswell <betidswell@vassar.edu>
2018-02-22 13:27:45 -0500

    Use pygame 2. Need to add updatef unctions in non pygame version

--- pygameReversi-2.py
+++ pygameReversi-2.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pygame
 
-boardSide = 10
+boardSide = 6
+empty = 0
 black = 1
 white = 2
+testSpot = 3
+legalSpot = 9
 
+pygame.init()
 
 (width, height) = (600, 600)
 screen = pygame.display.set_mode((width, height))
@@ -13,6 +17,8 @@ pygame.display.set_caption('Reversi')
 block_size = width/(boardSide+2)
 radius = block_size/3
 offset = (height-(block_size*boardSide))/2
+
+font = pygame.font.Font(None, int(offset/1.5))
 
 def setUp():
 	#for the board 0 is empty, 1 is black, and 2 is white
@@ -32,7 +38,7 @@ def displayBoard(board):
 
 def findSwitches(turn,other,array):
 
-	newPieceLoc = np.where(array == 3)[0][0]
+	newPieceLoc = np.where(array == testSpot)[0][0]
 
 	legalMove = False
 	changeLocs = []
@@ -49,7 +55,7 @@ def findSwitches(turn,other,array):
 		elif backArray[x] == turn:
 			foundTurn = True
 			break
-		elif backArray[x] == 0:
+		elif backArray[x] == empty  or backArray[x] == legalSpot:
 			break
 
 
@@ -65,7 +71,7 @@ def findSwitches(turn,other,array):
 		elif forwardArray[x] == turn:
 			foundTurn = True
 			break
-		elif forwardArray[x] == 0:
+		elif forwardArray[x] == empty or forwardArray[x] == legalSpot:
 			break
 
 	if foundTurn:
@@ -76,56 +82,68 @@ def findSwitches(turn,other,array):
 
 	return [legalMove,changeLocs]
 
+def findLegal(board,turn):
 
-def makeMove(board,move,turn):
-	if board[move[0]][move[1]] == black or board[move[0]][move[1]] == white:
-		print "Illegal Move: You cannot place a piece on another piece"
+	legalList = []
+
+	for x in range(boardSide):
+		for y in range(boardSide):
+			if board[x][y] != black and board[x][y] != white:
+				board[x][y] = testSpot
+
+				if turn == black:
+					other = white
+				else:
+					other = black
+
+				col = board[:,y]
+				row = board[x]
+				diagLeftRight = board.diagonal(y-x)
+				diagRightLeft = np.fliplr(board).diagonal(((boardSide-1)-y)-x)
+
+				if findSwitches(turn,other,col)[0] or findSwitches(turn,other,row)[0] or findSwitches(turn,other,diagLeftRight)[0] or findSwitches(turn,other,diagRightLeft)[0]:
+					legalList.append([x,y])
+					board[x][y] = legalSpot
+				else:
+					board[x][y] = 0
+
+	return legalList
+
+
+def makeMove(board,move,turn,legalList):
+	if move not in legalList:
+		print "Illegal Move"
 		return [turn,board]
 
-	board[move[0]][move[1]] = 3
+	board[move[0]][move[1]] = testSpot
 
 	if turn == black:
 		other = white
 	else:
 		other = black
 
-	legalMove = False
-
 	col = board[:,move[1]]
 	row = board[move[0]]
 	diagLeftRight = board.diagonal(move[1]-move[0])
 	diagRightLeft = np.fliplr(board).diagonal(((boardSide-1)-move[1])-move[0])
 
-	# print "Column",col
-	# print "Row",row
-	# print "Diag \\",diagLeftRight
-	# print "Diag /",diagRightLeft
+	colChanges = findSwitches(turn,other,col)[1]
+	rowChanges = findSwitches(turn,other,row)[1]
+	diagLeftRightChanges = findSwitches(turn,other,diagLeftRight)[1]
+	diagRightLeftChanges= findSwitches(turn,other,diagRightLeft)[1]
 
-	colResults = findSwitches(turn,other,col)
-	rowResults = findSwitches(turn,other,row)
-	diagLeftRightResults = findSwitches(turn,other,diagLeftRight)
-	diagRightLeftResults = findSwitches(turn,other,diagRightLeft)
-
-	legalMove = colResults[0] or rowResults[0] or diagLeftRightResults[0] or diagRightLeftResults[0]
-
-	if not legalMove:
-		print "Illegal Move: Nothing flipped"
-		board[move[0]][move[1]] = 0
-		return [turn,board]
-
-	for c in colResults[1]:
+	for c in colChanges:
 		board[move[0]+c][move[1]] = turn
 
-	for r in rowResults[1]:
+	for r in rowChanges:
 		board[move[0]][move[1]+r] = turn
 
-	for dlr in diagLeftRightResults[1]:
+	for dlr in diagLeftRightChanges:
 		board[move[0]+dlr][move[1]+dlr] = turn
 
-	for drl in diagRightLeftResults[1]:
+	for drl in diagRightLeftChanges:
 		board[move[0]+drl][move[1]-drl] = turn
 
-	
 	board[move[0]][move[1]] = turn
 
 	print str(turn)+"'s turn successful"
@@ -133,30 +151,46 @@ def makeMove(board,move,turn):
 
 def pygameDisplay(board):
 	screen.fill((255,255,255))
+
+	numBlack = 0
+	numWhite = 0
+
 	for x in range(boardSide):
 		for y in range(boardSide):
+			if board[x][y] == legalSpot:
+				color = (0,255,0)
+				size = 2
+			else:
+				color = (0,0,0)
+				size = 1
+
 			rect = pygame.Rect(offset+x*block_size,offset+y*block_size, block_size, block_size)
-			pygame.draw.rect(screen, (0,0,0), rect, 1)
+			pygame.draw.rect(screen, color, rect, size)
 
 	for x in range(boardSide):
 		for y in range(boardSide):
 			piece = board[x][y]
-			if piece == 1:
+			if piece == black:
+				numBlack += 1
 				pygame.draw.circle(screen, (0,0,0), (offset+(block_size/2)+x*block_size,offset+(block_size/2)+y*block_size), radius)
-			if piece == 2:
+			if piece == white:
+				numWhite += 1
 				pygame.draw.circle(screen, (0,0,0), (offset+(block_size/2)+x*block_size,offset+(block_size/2)+y*block_size), radius,2)
 
+	text = font.render("Black: "+str(numBlack)+" White: "+str(numWhite), 1, (0,0,0))
+	screen.blit(text,[width/2-offset*2,offset/3])
 
 
+
+running = True
+noMovesLast = False
+turn = black
 
 board = setUp()
-displayBoard(board)
-
+legalList = findLegal(board,turn)
 pygameDisplay(board)
 pygame.display.flip()
 
-running = True
-turn = black
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -165,7 +199,22 @@ while running:
 			pos = pygame.mouse.get_pos()
 			xLoc = (pos[0]-offset)/block_size
 			yLoc = (pos[1]-offset)/block_size
-			[turn,board] = makeMove(board,(xLoc,yLoc),turn)
+			[turn,board] = makeMove(board,[xLoc,yLoc],turn,legalList)
+			legalList = findLegal(board,turn)
+			if len(legalList) == 0:
+				if noMovesLast:
+					print "Game over"
+					running = False
+				else:
+					print "No Moves for",turn
+					noMovesLast = True
+					if turn == black:
+						turn = white
+					else:
+						turn = black
+			elif noMovesLast:
+				noMovesLast = False
+			legalList = findLegal(board,turn)
 			pygameDisplay(board)
 			pygame.display.flip() 
 
